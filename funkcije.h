fac02671794686e5f1618b99387f82fc330dc09c
Robi2510 <robert.juric11@gmail.com>
2024-06-13 14:13:54 +0200

    funkcije.h

---
+++ funkcije.h
@@ -0,0 +1,33 @@
+#ifndef FUNKCIJE_H
+#define FUNKCIJE_H
+
+#include <time.h>
+
+#define REDCI 3
+#define STUPCI 5
+#define POCETNI_KREDITI 1000
+#define MIN_ULOG 10
+
+typedef struct {
+	int stanjeRacuna;
+	int ulog;
+	int najveciDobitak;
+} Racun;
+
+typedef struct {
+	char simbol;
+	int dobitakZa3;
+	int dobitakZa4;
+	int dobitakZa5;
+} SimbolDobitak;
+
+void ispisiIzbornik();
+void igra(Racun* racun);
+void dodajKredite(Racun* racun);
+void promjeniUlog(Racun* racun);
+void prikaziStanje(const Racun* racun);
+int izracunajDobitak(char matrica[REDCI][STUPCI]);
+void ucitajStanje(Racun* racun);
+void spremiStanje(const Racun* racun);
+
+#endif // FUNKCIJE_H
