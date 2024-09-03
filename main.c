a90f41e9d448085826b6bcaa3a33505cf0fd5310
Robi2510 <robert.juric11@gmail.com>
2024-09-03 10:29:53 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,43 +1,47 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "funkcije.h"
 
 int main() {
-	srand(time(NULL)); // Inicijalizacija generatora slučajnih brojeva
-	Racun racun = { POCETNI_KREDITI, MIN_ULOG, 0 };
-	int opcija;
+    srand(time(NULL)); // Inicijalizacija generatora slučajnih brojeva
+    Racun racun = { POCETNI_KREDITI, MIN_ULOG, 0 };
+    int opcija;
 
-	ucitajStanje(&racun);
+    ucitajStanje(&racun);
 
-	do {
-		ispisiIzbornik();
-		scanf("%d", &opcija);
-		getchar();
+    do {
+        ispisiIzbornik();
+        scanf("%d", &opcija);
+        getchar();
 
-		switch (opcija) {
-		case 1:
-			igra(&racun);
-			break;
-		case 2:
-			dodajKredite(&racun);
-			break;
-		case 3:
-			promjeniUlog(&racun);
-			break;
-		case 4:
-			prikaziStanje(&racun);
-			break;
-		case 5:
-			printf("Izlaz...\n");
-			break;
-		default:
-			printf("Neispravan odabir, pokušajte ponovo.\n");
-		}
-	} while (opcija != 5);
+        switch (opcija) {
+        case 1:
+            igra(&racun);
+            break;
+        case 2:
+            dodajKredite(&racun);
+            break;
+        case 3:
+            promjeniUlog(&racun);
+            break;
+        case 4:
+            prikaziStanje(&racun);
+            break;
+        case 5:
+            spremiSortiraneDobitke();
+            break;
+        case 6:
+            obrisiDatotekuDobitaka();
+            break;
+        case 7:
+            printf("Izlaz...\n");
+            break;
+        default:
+            printf("Neispravan odabir, pokušajte ponovo.\n");
+        }
+    } while (opcija != 7);
 
-	spremiStanje(&racun);
+    spremiStanje(&racun);
 
-	return 0;
+    return 0;
 }
